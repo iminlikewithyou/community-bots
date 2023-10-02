e8b4c7bccc15bcd0046daba5dbe95398fc208fea
66minmin <christian@omg.games>
2023-10-01 23:08:34 -0500

    fix English formatting and update for new layout

--- lame-bot/commands/joinrulelottery.ts
+++ lame-bot/commands/joinrulelottery.ts
@@ -7,16 +7,23 @@ export const data = new SlashCommandBuilder()
   .setName("joinrulelottery")
   .setDescription("Join the regular rule lottery!");
 
+// similar to the one found in WBM, but this checks before the decimal point
+function isNumberStringVowelSound(x: string) {
+  return /^1[18]\.?/g.test(x) || x.startsWith("8");
+}
+
 export async function execute(interaction: CommandInteraction, preferBroadcast: boolean) {
   let lotteryEntry = await joinRegularRuleLottery(interaction.user.id);
 
-  if (lotteryEntry) {
+  if (lotteryEntry.currentEntry) {
+    let { entries, joinedAt } = lotteryEntry.currentEntry;
+    let chancePercentageString = (entries / lotteryEntry.totalEntries * 100).toFixed(0);
     await replyToInteraction(
       interaction,
       "Rule Lottery",
-      "\n- You're already in the rule lottery! You joined the lottery <t:" + Math.floor(lotteryEntry.joinedAt / 1000) + ":R>."
-      + `\n - You have **${formatNumber(lotteryEntry.entries)}** ${lotteryEntry.entries == 1 ? "entry" : "entries"}.`
-      + `\n - You have a **${(lotteryEntry.entries / lotteryEntry.totalEntries * 100).toFixed(1)}%** chance of being selected.`,
+      "\n- You're already in the rule lottery! You joined the lottery <t:" + Math.floor(joinedAt / 1000) + ":R>."
+      + `\n - You have **${formatNumber(entries)}** ${entries == 1 ? "entry" : "entries"}.`
+      + `\n - You have ${isNumberStringVowelSound(chancePercentageString) ? "an" : "a"} **${chancePercentageString}%** chance of being selected.`,
       false
     );
     return;
